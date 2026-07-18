d7ba2bb23683fcee95be6a5e28dce1cdc919e0d0
Harsh Vyas <134424925+Harshtitiya66@users.noreply.github.com>
2026-07-18 12:34:21 +0530

    add the hero changes

--- HarshT/src/components/Hero.jsx
+++ HarshT/src/components/Hero.jsx
@@ -7,16 +7,20 @@ function Hero() {
         {`@import url('https://fonts.googleapis.com/css2?family=VT323&display=swap');`}
       </style>
       
-      {/* 3D POV Perspective Lines */}
+      {/* 3D POV Perspective Lines & Cables */}
       <svg className="absolute inset-0 w-full h-full pointer-events-none z-0" xmlns="http://www.w3.org/2000/svg">
-        {/* Top Left */}
-        <line x1="0" y1="25%" x2="calc(50% - 200px)" y2="35%" stroke="#c2bcac" strokeWidth="3" strokeLinecap="square" />
-        {/* Top Right */}
-        <line x1="100%" y1="25%" x2="calc(50% + 200px)" y2="35%" stroke="#c2bcac" strokeWidth="3" strokeLinecap="square" />
-        {/* Bottom Left */}
-        <line x1="0" y1="90%" x2="calc(50% - 200px)" y2="70%" stroke="#c2bcac" strokeWidth="3" strokeLinecap="square" />
-        {/* Bottom Right */}
-        <line x1="100%" y1="90%" x2="calc(50% + 200px)" y2="70%" stroke="#c2bcac" strokeWidth="3" strokeLinecap="square" />
+        {/* Original Light Perspective Lines (Behind) */}
+        
+
+        {/* Wires tracing the perspective lines (as drawn by user) */}
+        {/* Left wire from board */}
+        <line x1="0" y1="25%" x2="calc(50% - 115px)" y2="35%" stroke="black" strokeWidth="4" strokeLinecap="square" />
+        {/* Right wire from board */}
+        <line x1="100%" y1="25%" x2="calc(50% + 115px)" y2="35%" stroke="black" strokeWidth="4" strokeLinecap="square" />
+        {/* Left wire from desk */}
+        <line x1="0" y1="90%" x2="calc(50% - 30px)" y2="70%" stroke="black" strokeWidth="4" strokeLinecap="square" />
+        {/* Right wire from desk (Red) */}
+        <line x1="100%" y1="90%" x2="calc(50% + 30px)" y2="70%" stroke="black" strokeWidth="4" strokeLinecap="square" />
       </svg>
 
       {/* Left Wall Text (Social Handles) */}
@@ -43,28 +47,23 @@ function Hero() {
       <div className="relative z-10 flex flex-col items-center mt-6">
         
         {/* Yellow Whiteboard Box (Pixel Style) */}
-        <div className="relative bg-[#fceb9e] border-4 border-[#2c2c2c] w-[350px] h-[120px] flex items-center justify-center shadow-[6px_6px_0px_0px_#2c2c2c]">
+        <div className="relative bg-[#fceb9e] border-4 border-[#2c2c2c] w-[350px] h-[120px] flex items-center justify-center shadow-[6px_6px_0px_0px_#2c2c2c] z-20">
           {/* Strings connecting to ceiling (Blocky) */}
           <div className="absolute bottom-full left-[20%] w-[4px] h-[300px] bg-[#2c2c2c]"></div>
           <div className="absolute bottom-full right-[20%] w-[4px] h-[300px] bg-[#2c2c2c]"></div>
 
-          <h1 className="text-[60px] text-[#2c2c2c] tracking-widest mt-2 uppercase">
+          <h1 className="text-[60px] text-[#2c2c2c] tracking-widest mt-2 uppercase relative">
             developer
+            {/* Blinking Cursor from the drawing */}
+            <span className="absolute -right-[20px] top-[10px] w-[4px] h-[50px] bg-[#2c2c2c] animate-pulse"></span>
           </h1>
         </div>
 
-        {/* Wires connecting whiteboard to the desk area (Pixel/Stair-step style) */}
-        <div className="w-[500px] h-[140px] relative mt-0 pointer-events-none z-20">
-          <svg className="w-full h-full" viewBox="0 0 500 140" fill="none" xmlns="http://www.w3.org/2000/svg">
-            {/* Left Wire (Dark Blue/Black) - Stair step */}
-            <path d="M 70 0 V 40 H 130 V 80 H 190 V 110 H 230 V 130" stroke="#1e293b" strokeWidth="6" fill="none" strokeLinecap="square" strokeLinejoin="miter" />
-            {/* Right Wire (Red) - Stair step */}
-            <path d="M 430 0 V 30 H 380 V 70 H 320 V 100 H 270 V 130" stroke="#991b1b" strokeWidth="6" fill="none" strokeLinecap="square" strokeLinejoin="miter" />
-          </svg>
-        </div>
+        {/* Center vertical wire from board to man */}
+        <div className="absolute top-[120px] left-1/2 -translate-x-1/2 w-[4px] h-[100px] bg-[#1e293b] -z-10"></div>
 
         {/* Developer Desk Image */}
-        <div className="relative -mt-[130px] z-10 pointer-events-none">
+        <div className="relative mt-8 z-10 pointer-events-none">
           <img 
             src="/bg_desktop_me-removebg-preview.png" 
             alt="Developer Desk" 
@@ -72,10 +71,9 @@ function Hero() {
             style={{ imageRendering: 'pixelated' }}
           />
         </div>
-
       </div>
     </div>
   )
 }
 
-export default Hero
+export default Hero
